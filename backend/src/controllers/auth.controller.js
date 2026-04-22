7ccc3decbf20beead167cb075c76b0c7178c157b
ShahzebJ <shahzeb.jadoon@acrosoft.io>
2026-04-22 16:22:48 +0500

    test husky

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -12,33 +12,40 @@ import bcrypt from "bcrypt";
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
-    if(!fullName || !email || !password) {
-      return res.status(400).json({ message: "All fields are required" });
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: " All fields are required" });
     }
-    if(password.length < 6) {
-      return res.status(400).json({ message: "Password must be at least 6 characters long" });
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
     }
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if(!emailRegex.test(email)) {
+    if (!emailRegex.test(email)) {
       return res.status(400).json({ message: "Invalid email" });
     }
     const existingUser = await User.findOne({ email });
-    if(existingUser) {
+    if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const newUser = await User.create({ fullName, email, password: hashedPassword });
+    const newUser = await User.create({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
     // generate JWT token
-    if(newUser) {
-        generateToken(newUser._id, res);
-        await newUser.save();    
-        res.status(201).json({ message: "User created successfully", user: newUser });
+    if (newUser) {
+      await newUser.save();
+      generateToken(newUser._id, res);
+      res
+        .status(201)
+        .json({ message: "User created successfully", user: newUser });
     } else {
-        return res.status(400).json({ message: "Failed to create user" });
+      return res.status(400).json({ message: "Failed to create user" });
     }
-    
   } catch (error) {
     console.error(error, "Error in signup controller");
     res.status(500).json({ message: "Internal server error" });
