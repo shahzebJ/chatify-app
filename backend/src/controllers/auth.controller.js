fa652932c1773977284d77b4f61bb1d1d303881f
ShahzebJ <shahzeb.jadoon@acrosoft.io>
2026-04-23 13:56:04 +0500

    auth backend completed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -67,10 +67,29 @@ export const signup = async (req, res) => {
 };
 
 export const signin = async (req, res) => {
-  return res.status(500).json({ message: "Not implemented" });
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ message: " All fields are required" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+    generateToken(user._id, res);
+    res.status(200).json({ message: "User logged in successfully", user });
+  } catch (error) {
+    console.error(error, "Error in signin controller");
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const signout = async (req, res) => {
   res.clearCookie("token");
+  res.clearCookie("jwt");
   res.status(200).json({ message: "Logged out successfully" });
 };
--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -2,11 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV = {
-  port: process.env.PORT || 5000,
-  clientUrl: process.env.CLIENT_URL || "http://localhost:3000",
-  mongodbUri: process.env.MONGODB_URI || "",
-  jwtSecret: process.env.JWT_SECRET || "",
-  resendApiKey: process.env.RESEND_API_KEY || "",
-  emailFrom: process.env.EMAIL_FROM || "",
-  emailFromName: process.env.EMAIL_FROM_NAME || "",
+  port: process.env.PORT,
+  clientUrl: process.env.CLIENT_URL,
+  mongodbUri: process.env.MONGODB_URI,
+  jwtSecret: process.env.JWT_SECRET,
+  resendApiKey: process.env.RESEND_API_KEY,
+  emailFrom: process.env.EMAIL_FROM,
+  emailFromName: process.env.EMAIL_FROM_NAME,
 };
